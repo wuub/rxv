f4f7e66e8123119ecc449760ead44512e7049065
Wojciech Bederski <github@wuub.net>
2013-11-24 00:55:13 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class Tox(TestCommand):
 setup(
     name='rxv',
     version='0.1.0',
-    description='Python source inspection with AST',
+    description='Automation Library for Yamaha RX-V473, RX-V573, RX-V673, RX-V773 receivers',
     long_description=open('README.rst').read(),
     author='Wojciech Bederski',
     url="https://github.com/wuub/rxv",

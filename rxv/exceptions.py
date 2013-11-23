c607cf255054ba34ca93cd91a177b6f35d4e9077
Wojciech Bederski <github@wuub.net>
2013-11-23 22:17:22 +0100

    move exceptions to separate file

---
+++ rxv/exceptions.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division, absolute_import, print_function
+
+class ReponseException(Exception):
+    """Exception raised when yamaha receiver responded with an error code"""
+    pass

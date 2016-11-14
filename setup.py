c62bff3da35cf61a4a8912450f110d1c62f4f919
Wojciech Bederski <github@wuub.net>
2016-11-14 14:41:50 +0100

    0.4.0 release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class Tox(TestCommand):
 
 setup(
     name='rxv',
-    version='0.3.1',
+    version='0.4.0',
     description='Automation Library for Yamaha RX-V473, RX-V573, RX-V673, RX-V773 receivers',
     long_description=open('README.rst').read(),
     author='Wojciech Bederski',

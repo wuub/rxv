26450af5ce006fbcc777f4806384681a7778b354
Wojciech Bederski <github@wuub.net>
2021-01-19 23:37:05 +0100

    remove testtools and fix deprecation warning

--- tests/test_surround.py
+++ tests/test_surround.py
@@ -1,9 +1,10 @@
-import testtools
+import unittest
 import requests_mock
 import rxv
 
 FAKE_IP = '10.0.0.0'
 DESC_XML = 'http://%s/YamahaRemoteControl/desc.xml' % FAKE_IP
+CTRL_URL = 'http://%s/YamahaRemoteControl/ctrl' % FAKE_IP
 
 
 def sample_content(name):
@@ -11,11 +12,11 @@ def sample_content(name):
         return f.read()
 
 
-class TestSurroundRXV(testtools.TestCase):
+class TestSurroundRXV(unittest.TestCase):
 
     @requests_mock.mock()
     def test_surround(self, m):
         m.get(DESC_XML, text=sample_content('rx-v473-desc.xml'))
-        rec = rxv.RXV(FAKE_IP)
+        rec = rxv.RXV(CTRL_URL)
         surround = rec.surround_programs()
         self.assertIn("Drama", surround)
